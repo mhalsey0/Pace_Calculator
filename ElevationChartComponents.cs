9e9ff15f1eefbf824010797fb38476d9b299b6fb
Mike Halsey <mhalsey0@gmail.com>
2024-08-08 10:00:44 -0400

    add mehtods for constucting chart elements from gpxfile

--- ElevationChartComponents.cs
+++ ElevationChartComponents.cs
@@ -11,6 +11,8 @@ namespace Pace_Calculator
         public List<double> Distance { get; set; }
         public List<double> Grade { get; set; }
         public List<int> Marker { get; set; }
+        public CalculatedInput CalculatedInput { get; set; }
+        
 
 
         public List<Coordinates> CreateCoordinatesFromGpxFile(GpxFile gpxFile)
@@ -41,6 +43,55 @@ namespace Pace_Calculator
             }
 
             return distanceBetweenPoints;
+        }
+
+        public List<double> GetElevationPoints(GpxFile gpxFile)
+        {
+            List<double> elevationPoints = new List<double>();
+
+            foreach (var point in gpxFile.Waypoints)
+            {
+                double elevation = (double)point.Elevation;
+                elevationPoints.Add(elevation);
+            }
+            return elevationPoints;
+        }
+
+        public List<double> GetGradeBetweenPoints(GpxFile gpxFile, CalculatedInput calculatedInput)
+        {
+            List<double> grade = new List<double>();
+            List<double> distanceBetweenPoints = GetDistanceBetweenPointsFromGpxFile(gpxFile, calculatedInput);
+            List<double> elevationPoints = GetElevationPoints(gpxFile);
+            elevationPoints.RemoveAt(elevationPoints.Count - 1);
+
+            for (int i = 0; i < elevationPoints.Count;)
+            {
+                if(distanceBetweenPoints[i] == 0)
+                {
+                    grade.Add(double.NaN);
+                }
+                grade.Add(elevationPoints[i] / distanceBetweenPoints [i]);
+            }
+            return grade;
+        }
+
+        public List<int> GetMarkersFromGpxFile(GpxFile gpxFile, CalculatedInput calculatedInput)
+        {
+            List<int> markers = new List<int>();
+            UnitOfLength unitOfLength = calculatedInput.Unit;
+            List<Coordinates> coordinates = CreateCoordinatesFromGpxFile(gpxFile);
+            Coordinates start = coordinates[0];
+            Coordinates end = coordinates[^1];
+            double totalDistance = CoordinatesDistanceExtensions.DistanceTo(start, end, calculatedInput.Unit);
+
+            for (int mark = 0; mark < totalDistance+1;)
+            {
+                markers.Add(mark);
+            }
+
+            return markers;
+
+
         }
 
     }
